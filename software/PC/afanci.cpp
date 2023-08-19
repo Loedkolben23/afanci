d2174c107f220bb5dcf949819e7b4010d4da1ee9
Brabant Paul <paul.brabant2007@gmail.com>
2023-08-19 16:10:05 +0200

    rm

--- software/PC/afanci.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-    //if (argc > 1) 
-//        return 1; 
-    std::cout << "TEST" << std::endl;
-    return 0;
-}
--- software/PC/out/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/paulb/Documents/projects/afanci/software/PC
-# Build directory: C:/Users/paulb/Documents/projects/afanci/software/PC/out/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1_1 "C:/Users/paulb/Documents/projects/afanci/software/PC/out/build/afanci")
-set_tests_properties(test1_1 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/paulb/Documents/projects/afanci/software/PC/CMakeLists.txt;21;add_test;C:/Users/paulb/Documents/projects/afanci/software/PC/CMakeLists.txt;0;")
