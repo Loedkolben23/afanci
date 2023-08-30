429e6e064fe35c5744b228add2b5332515cae178
Brabant Paul <paul.brabant2007@gmail.com>
2023-08-30 17:05:14 +0200

    set up project for atmel

---
+++ software/ATMEGA/afanci/afanci/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 8/30/2023 8:13:33 AM
+ *  Author: paulb
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
