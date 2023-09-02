313c8bcd4cd61ba5ff77810343620f1c4e79b695
Brabant Paul <paul.brabant2007@gmail.com>
2023-09-02 23:22:36 +0200

    pwm

--- software/ATMEGA/afanci/afanci/main.c
+++ software/ATMEGA/afanci/afanci/main.c
@@ -2,35 +2,32 @@
  * main.c
  *
  * Created: 8/30/2023 8:13:33 AM
- *  Author: paulb, Qualspirale
+ *  Author: paulb
  */ 
+
+#define F_CPU  160000000
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-ISR (TIMER1_OVR_vect)
-{
-	PORTB = 000000001;	
-}
+double pwm_signal_percent = 0; 
 
-int main(void) {
-	// PIN CONFIG
-	DDRB = 000000001;
+int main(void){
+	//TCCR0A – Timer/Counter Control Register A; sets timer as "PWM Timer"
+	TCCR0A = (1 << WGM00) | (1 << COM0A1);
+	TCCR0B = (1 << CS01); //00000010 clk/8
+	DDRD = 0xFF;
+	sei();
 	
-	TCNT1 = 0; 
-	TCCR1B = (1 << CS10) | (1 << CS12);
+	OCR0A = (pwm_signal_percent/100)*255;
 	
-	TCCR1A = 00x0;
-	
-	TIMSK1 = (1 << TOIE1);
-	
-	sei():
-	
-	
-PORTB = 000000001;	
-
-	while(1) {}
-	return 0;
+	while(1)
+	{
+		
+		OCR0A = 100;
+		_delay_ms(500);
+		OCR0A = 0;
+		_delay_ms(500);
 	}
-
-//8 -> PB0
+}
