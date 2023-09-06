cacee7077107029ddb6474ad5ebfd5a3cd2a5950
Brabant Paul <paul.brabant2007@gmail.com>
2023-09-06 19:46:07 +0200

    fancurve

--- software/ATMEGA/afanci/afanci/test.c
+++ software/ATMEGA/afanci/afanci/test.c
@@ -7,13 +7,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
-
-#define F_CPU	160000000
-#define BUAD	9600
-#define BRC		((F_CPU/16/BUAD) -1)
-
-
 #include <util/delay.h>
+
+const uint8_t fanCurve[][2] = {
+	{25, 0},  // Temperature (°C), Fan Percentage
+	{30, 25},
+	{35, 50},
+	{40, 75},
+	{45, 100}
+};
+
+#define FAN_CURVE_SIZE (sizeof(fanCurve) / sizeof(fanCurve[0]))
+
 
 void initPWM() {
 	// Configure Timer0 for Fast PWM on OC0A (Pin 6) and OC0B (Pin 5)
@@ -31,40 +36,52 @@ void initPWM() {
 	// Set PWM pins as output
 	DDRD = (1 << PD6) | (1 << PD5)| (1 << PD3); // OC0A (Pin 6) and OC0B (Pin 5),  OC2B (Pin 3)
 	DDRB = (1 << PB1) | (1 << PB2)| (1 << PB3); // OC1A (Pin 9) and OC1B (Pin 10) (PIN 11) 
-	
-
 }
 
-void writing_eeprom(){
-	//7.4 Documentation   
-	//EEPROM can be corrupted if vcc is to low!! 
+uint8_t CalculateFanPercentage(uint16_t temperature) {
+	uint8_t closestPercentage = 0;
+	uint16_t closestTempDiff = UINT16_MAX;
+
+	for (int i = 0; i < FAN_CURVE_SIZE; i++) {
+		uint16_t tempDiff = abs(temperature - fanCurve[i][0]);
+		if (tempDiff < closestTempDiff) {
+			closestTempDiff = tempDiff;
+			closestPercentage = fanCurve[i][1];
+		}
 	}
-	
-void reading_eeprom(){
-	
+
+	return closestPercentage;
 }
 
 int main() {
 	initPWM();
-	UBRR0H = (BRC >> 8);
-	UBRR0L = BRC;
-		
-	UCSR0B = (1 << TXEN0);
-	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-
-
 	while (1) {
 		// Adjust PWM duty cycles as needed
-		OCR0A = 255; // Duty cycle for OC0A (Pin 6)
-		OCR0B = 255;  // Duty cycle for OC0B (Pin 5)
-		OCR1A = 255; // Duty cycle for OC1A (Pin 9)
-		OCR1B = 255;  // Duty cycle for OC1B (Pin 10)
-		OCR2A = 255;  // Duty cycle for OC2A (Pin 11)
-		OCR2B = 255; // Duty cycle for OC2B (Pin 3)
-		//writes into data register
-		UDR0  = '8';
+		//OCR0A = 255; // Duty cycle for OC0A (Pin 6)
+		//OCR0B = 255;  // Duty cycle for OC0B (Pin 5)
+		//OCR1A = 255; // Duty cycle for OC1A (Pin 9)
+		//OCR1B = 255;  // Duty cycle for OC1B (Pin 10)
+		//OCR2A = 255;  // Duty cycle for OC2A (Pin 11)
+		//OCR2B = 255; // Duty cycle for OC2B (Pin 3)
+		uint16_t temperature = 46;
+
+		// Calculate fan percentage using the fan curve
+		uint8_t fanPercentage = CalculateFanPercentage(temperature);
+
+		// Convert fan percentage to PWM value (assuming linear mapping from 0-100% to 0-255)
+		uint8_t pwmValue = (fanPercentage * 255) / 100;
+
+		OCR0A = pwmValue; // Duty cycle for OC0A (Pin 6)
+		OCR0B = pwmValue;  // Duty cycle for OC0B (Pin 5)
+		OCR1A = pwmValue; // Duty cycle for OC1A (Pin 9)
+		OCR1B = pwmValue;  // Duty cycle for OC1B (Pin 10)
+		OCR2A = pwmValue;  // Duty cycle for OC2A (Pin 11)
+		OCR2B = pwmValue; // Duty cycle for OC2B (Pin 3)
+
 		_delay_ms(100); // Delay for demonstration
 	}
 }
 
+ 
+ 
  
